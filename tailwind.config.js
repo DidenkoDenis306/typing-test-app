4c158b9af923b5afb1cc0a7e9136ecec3f2cae36
Денис Диденко <101824489+DidenkoDenis306@users.noreply.github.com>
2023-07-11 14:35:48 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        timberwolf: '#dad7cd',
+        sage: '#a3b18a',
+        'fern': '#588157',
+        'hunter': '#3a5a40',
+        'brunswick': '#344e41',
+      }
+    },
   },
   plugins: [],
 }
